d904304111e2e924d223179a448ba7ca55aced4c
karimzahrouni <35636080+karimzahrouni@users.noreply.github.com>
2019-03-04 17:40:57 +0100

    Rename teste_capteure_ultrasonique.ino to teste capteure ultrasonic/teste_capteure_ultrasonique.ino

---
+++ teste capteure ultrasonic/teste_capteure_ultrasonique.ino
@@ -0,0 +1,25 @@
+int trig = 12; 
+int echo = 13; 
+long lecture_echo; 
+long cm;
+
+void setup() 
+{ 
+  pinMode(trig, OUTPUT); 
+  digitalWrite(trig, LOW); 
+  pinMode(echo, INPUT); 
+  Serial.begin(9600); 
+}
+
+void loop() 
+{ 
+  digitalWrite(trig, HIGH); 
+  delayMicroseconds(10); 
+  digitalWrite(trig, LOW); 
+  lecture_echo = pulseIn(echo, HIGH); // dureé d'impulsion !!!0.
+  cm = lecture_echo / 58; 
+  Serial.print("Distancem : "); 
+  Serial.println(cm); 
+  delay(1000);
+
+}
